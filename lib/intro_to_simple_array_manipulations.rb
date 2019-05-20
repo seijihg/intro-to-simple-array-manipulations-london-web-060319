b8129865979650ab1bbdd242591422983478c0fc
Le H Ngo <seiji@o2.pl>
2019-05-20 16:47:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(arr, str)
+  arr.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
